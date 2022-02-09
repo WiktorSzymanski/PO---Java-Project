3e270aac11146dd21d6e45ab0f57243ba1766606
DarkVikos <darkvikos@gmail.com>
2022-02-10 00:03:40 +0100

    add feat: saving and loading (multiple files)

--- src/pl/vik/gm/save_load/LoadGame.java
+++ src/pl/vik/gm/save_load/LoadGame.java
@@ -2,24 +2,67 @@ package pl.vik.gm.save_load;
 
 import pl.vik.gm.GameData;
 
+import java.beans.beancontext.BeanContextChild;
 import java.io.*;
+import java.util.HashMap;
 
 public class LoadGame {
-    String filePatch = "saves/file.txt";
     DataInputStream inputStream = null;
 
     GameData gameData = GameData.getInstance();
 
     public LoadGame() throws IOException {
+        String fileName = selectFile(getFilesMap());
+
+        load(fileName);
+    }
+
+    private void load(String fileName) throws IOException {
+        String filePatch = "saves/" + fileName;
+
         try {
             inputStream = new DataInputStream(new FileInputStream(filePatch));
             gameData.highestLevelComplited = inputStream.readInt();
         } finally {
             if(inputStream != null) {
                 inputStream.close();
+                System.out.println("Game Loaded");
+            }
+        }
+    }
+
+    private void printMap(HashMap<Integer,String> filesNames) {
+        for (Integer i : filesNames.keySet()) {
+            System.out.println(i + ". " + filesNames.get(i));
+        }
+    }
+
+    private String selectFile(HashMap<Integer,String> filesNames) {
+        printMap(filesNames);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int action = 0;
+        try {
+            action = Integer.parseInt(input.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return filesNames.get(action);
+    }
+
+    private HashMap<Integer, String> getFilesMap() {
+        File folder = new File("saves");
+        File[] listOfFiles = folder.listFiles();
+
+        HashMap<Integer,String> filesNames = new HashMap<>();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                filesNames.put(i+1,listOfFiles[i].getName());
             }
         }
 
-        System.out.println("Game Loaded");
+        return filesNames;
     }
 }
--- src/pl/vik/gm/save_load/SaveGame.java
+++ src/pl/vik/gm/save_load/SaveGame.java
@@ -2,18 +2,18 @@ package pl.vik.gm.save_load;
 
 import pl.vik.gm.GameData;
 
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 
 public class SaveGame {
-    String filePatch = "saves/file.txt";
+    String filePatch = "saves/";
     DataOutputStream outputStream = null;
 
     GameData gameData = GameData.getInstance();
 
     public SaveGame() throws IOException {
+        filePatch += getFileName();
+
         try {
             outputStream = new DataOutputStream(new FileOutputStream(filePatch));
             outputStream.writeInt(gameData.highestLevelComplited);
@@ -25,4 +25,20 @@ public class SaveGame {
 
         System.out.println("Game Saved");
     }
+
+    private String getFileName() {
+        String fileName = null;
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            fileName = input.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return fileName;
+    }
 }
+
+
